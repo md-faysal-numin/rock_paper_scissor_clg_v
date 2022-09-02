069e1821baa44d448937fcb56a33ba1a9c478065
Faysal <mdfaysalnumin@gmail.com>
2022-09-02 16:58:14 +0600

    UI added

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
 
+
 const types = ['ROCK','PAPER','SCISSOR'];
+
+
+const btns  = document.querySelectorAll('.btn');
+const playerPointEl = document.querySelector('.playerPoint');
+const computerPointEl = document.querySelector('.computerPoint');
+const updateRoundEl = document.querySelector('.updateRound');
+const mainResultEl = document.querySelector('.mainResult');
+
+
+
+
 
 let playerScore = 0;
 let computerScore = 0;
 const computerSelection = computerPlay();
 
-game();
-function game()
-{
-    for(let i=0;i<5;i++)
-    {
-        const playerSelection = prompt("Enter Your Choice").toUpperCase();
+// game();
+// function game()
+// {
+//     for(let i=0;i<5;i++)
+//     {
+//         const playerSelection = prompt("Enter Your Choice").toUpperCase();
 
-        console.log(playGround(playerSelection,computerSelection));
-    }
-    if(playerScore>computerScore)
-    {
-        console.log("You Won The Game!");
-    }
-    else
-    {console.log("You Lose The Game!");
+//         console.log(playGround(playerSelection,computerSelection));
+//     }
+//     if(playerScore>computerScore)
+//     {
+//         console.log("You Won The Game!");
+//     }
+//     else
+//     {console.log("You Lose The Game!");
 
-    }
-}
+//     }
+// }
+btns.forEach(btn=>{
+    btn.addEventListener('click',()=>{
+        let playerChoice = (btn.innerHTML).toUpperCase();
+        // console.log(playerChoice);
+        let computerChoice = computerPlay();
+       updateRoundEl.textContent= playGround(playerChoice,computerChoice);
+    })
+})
 function playGround(player,computer)
 {
     if(player === 'ROCK')
@@ -31,12 +51,18 @@ function playGround(player,computer)
         {
 
             playerScore++;
-            return "You Win! Rock beats scissor";
+            updateScoreDisplay();
+           return "You Win! Rock beats scissor";
+        }
+        else if(player === 'PAPER')
+        {
+            computerScore++;
+            updateScoreDisplay()
+            return "You Lose! paper beats rock";
         }
         else
         {
-            computerScore++;
-            return "You Lose!  beats paper";
+            return "Tie";
         }
     }
     
@@ -45,11 +71,18 @@ function playGround(player,computer)
         if(computer === 'ROCK'){
 
             playerScore++;
+            updateScoreDisplay()
             return "You Win! paper beats rock";
         }
-        else
+        else if(computer === 'SCISSOR')
         {computerScore++; 
-                return "You Lose! rock beats scissor";
+            updateScoreDisplay()
+                return "You Lose! scissor beats paper";
+
+        }
+        else
+        {
+            return "Tie";
 
         }
     }
@@ -61,12 +94,18 @@ function playGround(player,computer)
         {
 
             playerScore++;
+            updateScoreDisplay()
             return "You Win! scissor beats paper";
+        }
+        else if(computer=== 'ROCK')
+        {
+            computerScore++;
+            updateScoreDisplay()
+            return "You Lose! rock beats scissor";
         }
         else
         {
-            computerScore++;
-            return "You Lose! paper beats rock";
+            return "Tie";
         }
     }
    
@@ -78,4 +117,22 @@ function playGround(player,computer)
 function computerPlay()
 {
     return types[Math.floor(3*Math.random())];
+}
+
+function updateScoreDisplay()
+{
+
+    playerPointEl.textContent = playerScore;
+    computerPointEl.textContent = computerScore;
+    if(playerScore==5 || computerScore==5)
+    {
+        if(playerScore==5)
+        {
+            alert("You Won The Game!");
+        }
+        else{
+                   alert("You Lose The Game!");
+        }
+        location.reload();
+    }
 }
